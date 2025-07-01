7d2edf7e091ce9c2313d316b5fd4f56d8ff62684
Kenzen92 <jamespeterkenny@googlemail.com>
2025-07-01 15:06:57 +0100

    update vite config with preview url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+    allowedHosts: [
+      'kennysolutions-8cf06dbf8504.herokuapp.com'
+    ]
+  },
 })
