52c56a57cdcd54db1f2e2196b832901d2f18da0a
Kenzen92 <jamespeterkenny@gmail.com>
2025-10-27 16:47:26 +0000

    fix line endings

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
+﻿#!/usr/bin/env bash
 set -e
 
-echo "🛠 Environment: $ENVIRONMENT"
+echo "ðŸ›  Environment: $ENVIRONMENT"
 
 cd /app/kennysolutions
 
@@ -11,9 +11,10 @@ poetry run python manage.py migrate
 # Collect static files in production
 if [ "$ENVIRONMENT" = "production" ]; then
   poetry run python manage.py collectstatic --noinput
-  echo "🚀 Starting Gunicorn..."
+  echo "ðŸš€ Starting Gunicorn..."
   exec poetry run gunicorn kennysolutions.wsgi:application --bind 0.0.0.0:8000
 else
-  echo "🚀 Starting Django development server..."
+  echo "ðŸš€ Starting Django development server..."
   exec poetry run python manage.py runserver 0.0.0.0:8000
 fi
+
